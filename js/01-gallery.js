2503d3ee5260e8c2085f7a60e919f148f423637f
Julia <juliasuchomlyn@gmail.com>
2022-07-02 12:25:12 +0300

    add task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryBoxEl = document.querySelector('.gallery');
+galleryBoxEl.insertAdjacentHTML('beforeend', galleryCreate(galleryItems));
+
+
+function galleryCreate() {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original.value}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    }).join('');
+};
+
+const clickImage = (e) => {
+    e.preventDefault();
+
+    if (e.target.classList.contains('gallery')) return;
+    const source = e.target.dataset.source;
+
+    const instance = basicLightbox.create(`<img src="${source}" width="800" heigth="600">`)
+    instance.show();
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape') instance.close()
+    });
+}
+galleryBoxEl.addEventListener('click', clickImage)
 
 console.log(galleryItems);
